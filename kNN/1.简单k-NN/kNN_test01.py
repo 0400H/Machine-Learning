74c544f17de3ab62ccc59c3ac3d33cbe7c25baec
0400h <github@0400h.cn>
2019-02-27 01:00:17 +0800

    fix code style

--- kNN/1.简单k-NN/kNN_test01.py
+++ kNN/1.简单k-NN/kNN_test01.py
@@ -24,10 +24,10 @@ def ReadDataSetCSV(file, start, end):
 '''
     函数说明:kNN算法,分类器
     Parameters:
-        testcase     - 用于分类的数据(测试集)
-        dataSet - 用于训练的数据(训练集)
-        labes   - 分类标签
-        k       - kNN算法参数,选择距离最小的k个点
+        testcase - 用于分类的数据(测试集)
+        dataSet  - 用于训练的数据(训练集)
+        labes    - 分类标签
+        k        - kNN算法参数,选择距离最小的k个点
     Returns:
         sortedClassCount[0][0] - 分类结果
 '''
